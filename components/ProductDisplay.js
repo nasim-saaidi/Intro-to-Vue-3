f2e7abb5b35e712389c7f2a7bb8c0bbcebc3bef9
limino <115530133+nasim-saaidi@users.noreply.github.com>
2024-09-05 16:30:20 +0200

    finished

--- components/ProductDisplay.js
+++ components/ProductDisplay.js
@@ -22,7 +22,6 @@ app.component('product-display', {
         <ul>
           <li v-for="detail in details">{{ detail }}</li>
         </ul>
-
         <div 
           v-for="(variant, index) in variants" 
           :key="variant.id" 
@@ -41,6 +40,8 @@ app.component('product-display', {
 
       </div>
     </div>
+    <review-list v-if="reviews.length"  :reviews="reviews" ></review-list>
+    <review-form @review-submitted="addReview"></review-form>
   </div>`,
   data() {
     return {
@@ -51,7 +52,8 @@ app.component('product-display', {
         variants: [
           { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
           { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
-        ]
+        ],
+        reviews: []
     }
   },
   methods: {
@@ -60,7 +62,10 @@ app.component('product-display', {
       },
       updateVariant(index) {
           this.selectedVariant = index
-      }
+      },
+      addReview(review) {
+        this.reviews.push(review)
+      },
   },
   computed: {
       title() {
@@ -77,6 +82,7 @@ app.component('product-display', {
           return 'Free'
         }
         return 2.99
-      }
+      },
+      
   }
 })
